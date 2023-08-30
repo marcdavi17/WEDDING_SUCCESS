9edf8ab2bababa3cdfcbc1950e5fbc587f07ec64
Marc Davi <techthelaw@gmail.com>
2023-08-30 15:13:48 +0200

    work on guests

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -4,4 +4,19 @@ class FamiliesController < ApplicationController
     @families = Family.all
   end
 
+  def new
+    @family = Family.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @family = Family.new(family_params)
+    @family.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to action: "index"
+  end
+
+  private
+
+  def family_params
+    params.require(:family).permit(:name, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#dashboard"
+  resources :guests
+  resources :families
   get 'dashboard', to: 'pages#dashboard', as: 'dashboard'
-  get "guests", to: "guests#index"
-  get "families", to: "families#index"
-  get "guest/new", to: "guest#new"
+  # get "guests", to: "guests#index"
+  # get "guest/new", to: "guest#new"
+  # get "families", to: "families#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
