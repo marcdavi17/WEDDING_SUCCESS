db96e4bdc2cfb68e85049f60cc31ded4f129043c
Marc Davi <techthelaw@gmail.com>
2023-09-01 12:00:55 +0200

    pre demo day

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,10 +1,12 @@
 class Guest < ApplicationRecord
   belongs_to :family
-  belongs_to :spouse, optional: true
+  belongs_to :spouse
   # belongs_to :guest_desk
   # belongs_to :desk, through: :guest_desk
   has_many :guest_desks, dependent: :destroy
   has_many :desks, through: :guest_desk
 
   validates :first_name, presence: true
+
+  GENDER = ["woman", "man", "other"]
 end
