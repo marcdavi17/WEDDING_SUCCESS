f51887b34a776bfada6eaca13fd6e2847b743eeb
rouguietou kande <rouguirka@gmail.com>
2023-09-05 16:45:05 +0200

    map done

--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -4,4 +4,7 @@ class Wedding < ApplicationRecord
   has_many :desks, dependent: :destroy
   has_many :families, through: :guests
   has_many :guest_desks, through: :desks
+
+  geocoded_by :city_hall
+  after_validation :geocode, if: :will_save_change_to_city_hall?
 end
